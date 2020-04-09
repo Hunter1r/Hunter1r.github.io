08ffd75d3425d96fcaf02241bcc1e3c31850e8f0
Hunter1r <Hunter1r@yandex.ru>
2020-04-10 01:37:51 +0500

    Доработка слайдера в шапке сайта. -Пролистывание фона влево/вправо стрелками -Пролистывание фона влево/вправо кликая на пагинацию -Автопролистывание

--- scripts/script.js
+++ scripts/script.js
@@ -4,30 +4,59 @@ let video = document.querySelector('.video-pres-video');
 let leftArrow = document.querySelector('.header-bottom-left');
 let rightArrow = document.querySelector('.header-bottom-right');
 let headerContainer = document.querySelector('.header-container');
-let imgs = ['img/header-photo.png', 'img/header-mountain.jpg', 'img/header-alps.jpg'];
+let imgs = ['img/header-photo.png', 'img/header-mountain.jpg', 'img/header-alps.jpg', 'img/header-monument_valley.jpg', 'img/header-forest.jpg'];
 let indexImg = 0;
 let intervalId = null;
+let numb = 1;
+let pagination = document.querySelectorAll('.pagination-link');
+pagination.forEach(pagEl => {
+    pagEl.setAttribute('data-pagnumber', numb);
+    pagEl.addEventListener('click', event => {
+
+        headerContainer.style.backgroundImage = `url(${imgs[event.target.dataset.pagnumber - 1]})`;
+        resetPagStyle();
+        event.target.style.border = '2px solid #d3c1ad';
+        event.target.style.backgroundColor = 'transparent';
+        indexImg = event.target.dataset.pagnumber - 1;
+    });
+    numb++;
+})
+
+function resetPagStyle() {
+    pagination.forEach(pag => {
+        pag.style.border = '2px solid transparent';
+        pag.style.backgroundColor = '#fff';
+    })
+}
+
 leftArrow.addEventListener('click', function () {
-    if(typeof(intervalId) == 'number'){
+    if (typeof (intervalId) == 'number') {
         window.clearInterval(intervalId);
         intervalId = null;
     }
-    if (indexImg < 0) { indexImg = imgs.length - 1 }
+    if (indexImg <= 0) { indexImg = imgs.length - 1 
+    }else{
+        indexImg--;
+    }
     headerContainer.style.backgroundImage = `url(${imgs[indexImg]})`;
-    indexImg--;
+    
+    setStylePagination();
+    
 });
 
 rightArrow.addEventListener('click', function () {
-    
-    if(typeof(intervalId) == 'number'){
+
+    if (typeof (intervalId) == 'number') {
         window.clearInterval(intervalId);
         intervalId = null;
     }
-    if (indexImg > 2) { 
-        indexImg = 0; 
-    }
+    
+    if (indexImg >= 4) {
+        indexImg = 0;
+    }else{indexImg++;}
     headerContainer.style.backgroundImage = `url(${imgs[indexImg]})`;
-    indexImg++;
+    setStylePagination();
+    
 })
 
 videoSvg.addEventListener('click', function () {
@@ -51,19 +80,31 @@ window.addEventListener('scroll', function (event) {
 })
 
 window.addEventListener('load', function () {
+    
+    pagination[0].style.border = '2px solid #d3c1ad';
+    pagination[0].style.backgroundColor = 'transparent';
     //start sliding header img's
     intervalId = window.setInterval(changeImg, 3000);
 })
 
 function changeImg() {
 
-    if (indexImg > 2) { indexImg = 0; }
+    if (indexImg >= 4) { indexImg = 0; }else{indexImg++;}
     headerContainer.style.backgroundImage = `url(${imgs[indexImg]})`;
-    if (indexImg > 0) {
-        headerContainer.style.opacity = 0.8;
-    } else { headerContainer.style.opacity = 1; }
-    indexImg++;
-
+    setStylePagination();
     
+}
+
+function setStylePagination() {
+    //сброс стилей у пагинации в стиль "по умолчанию"
+    resetPagStyle();
+    pagination.forEach(pagEl => {
+
+        if (pagEl.dataset.pagnumber - 1 == indexImg) {
+            //установка стиля только у одной "кнопки" пагинации
+            pagEl.style.border = '2px solid #d3c1ad';
+            pagEl.style.backgroundColor = 'transparent';
+        }
+    })
 
 }
